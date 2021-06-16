dc48646cc818f93650b0320dc996659190fb6819
Marouane Moumni <MAR@YC.com>
2021-06-16 08:56:28 +0100

    Fetch Added

---
+++ ConsumeAPI/ConsumeAPI/ConsumeAPI/Helper/Services.cs
@@ -0,0 +1,64 @@
+﻿using ConsumeAPI.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace ConsumeAPI
+{
+    public class WebAPIService
+    {
+        #region Fields 
+
+        System.Net.Http.HttpClient client;
+
+        #endregion
+
+        #region Properties 
+
+        public ObservableCollection<ProductsModel> Items
+        {
+            get; private set;
+        }
+
+        public string WebAPIUrl
+        {
+            get; private set;
+        }
+
+        #endregion
+
+        #region Constructor
+        public WebAPIService()
+        {
+            client = new System.Net.Http.HttpClient();
+        }
+
+        #endregion
+
+        #region Methods
+        public async System.Threading.Tasks.Task<ObservableCollection<ProductsModel>> RefreshDataAsync()
+        {
+            WebAPIUrl = "https://productapiccc.herokuapp.com/api/product"; 
+            var uri = new Uri(WebAPIUrl);
+            try
+            {
+                var response = await client.GetAsync(uri);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    Items = JsonConvert.DeserializeObject<ObservableCollection<ProductsModel>>(content);
+                    return Items;
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            return null;
+        }
+
+        #endregion
+    }
+}
---
+++ ConsumeAPI/ConsumeAPI/ConsumeAPI/ViewModel/ProductsViewModel.cs
@@ -0,0 +1,57 @@
+﻿using ConsumeAPI.Models;
+using ConsumeAPI;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+
+namespace ConsumeAPI
+{
+    public class ProductsViewModel : INotifyPropertyChanged
+    {
+        #region Fields
+
+        WebAPIService webAPIService;
+        public event PropertyChangedEventHandler PropertyChanged;
+        private ObservableCollection<ProductsModel> items;
+
+        #endregion
+
+        #region Properties
+        public ObservableCollection<ProductsModel> Items
+        {
+            get
+            {
+                return items;
+            }
+            set
+            {
+                items = value;
+                RaisepropertyChanged("Items");
+            }
+        }
+        #endregion
+
+        #region Constructor
+        public ProductsViewModel()
+        {
+            webAPIService = new WebAPIService();
+            Items = new ObservableCollection<ProductsModel>();
+            GetDataFromWebAPI();
+        }
+        #endregion
+
+        #region Methods 
+        async void GetDataFromWebAPI()
+        {
+            Items = await webAPIService.RefreshDataAsync();
+        }
+        void RaisepropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
